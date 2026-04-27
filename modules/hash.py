ff041bb0ec74eb133565b25565b48d035cfae014
alexmadrinanfdez <a.madrinanfer@gmail.com>
2026-04-28 00:14:03 +0200

    hash file verification

--- modules/hash.py
+++ modules/hash.py
@@ -9,5 +9,16 @@ def hash_file(file_path):
               chunk = file.read(1024)
     return h.hexdigest()
 
+def verify_integrity(orig_file, dest_file):
+    orig_hash = hash_file(orig_file)
+    dest_hash = hash_file(dest_file)
+    print(f"Integrity check between {orig_file} and {dest_file}:")
+    if orig_hash == dest_hash:
+        return "Files are identical."
+    return "Files are different."
+
 if __name__ == "__main__":
     print("The SHA-256 hash of the file is:", hash_file("sample_files/sample.txt"))
+
+    print(verify_integrity("sample_files/python-powered-w.svg", "sample_files/python-powered-h.svg"))
+    print(verify_integrity("sample_files/python-powered-w.svg", "sample_files/python-powered-w-copy.svg"))
