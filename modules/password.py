a6a1704b1dfd1dc46a734ba044e96c9d425cd22e
alexmadrinanfdez <a.madrinanfer@gmail.com>
2026-04-29 10:35:44 +0200

    improve password input

--- modules/password.py
+++ modules/password.py
@@ -1,4 +1,5 @@
 from zxcvbn import zxcvbn
+from getpass import getpass
 
 def check_password_strength(password: str) -> tuple[int, dict]:
     result = zxcvbn(password)
@@ -12,7 +13,13 @@ def parse_feedback(feedback: dict) -> str:
     return f"Warning: {warning}\nSuggestions: {'; '.join(suggestions)}"
 
 if __name__ == "__main__":
-    password = input("Enter a password to check its strength: ")
-    score, feedback = check_password_strength(password)
-    print(f"Password Strength Score (0-4): {score}")
-    print(parse_feedback(feedback))
+    strength_threshold = 3
+    while True:
+        password = getpass("Enter a password to check its strength: ", echo_char='*')
+        score, feedback = check_password_strength(password)
+        print(f"Password Strength Score (0-4): {score}")
+        print(parse_feedback(feedback))
+        
+        if score >= strength_threshold:
+            break
+        print("Please try again with a stronger password.\n")
