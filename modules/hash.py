5735aeb47dbf6c9cdb98ebd9a1256e68a5c83ba7
alexmadrinanfdez <a.madrinanfer@gmail.com>
2026-05-04 09:28:02 -0500

    file verification return type change

--- modules/hash.py
+++ modules/hash.py
@@ -1,6 +1,6 @@
 import hashlib
 
-def hash_file(file_path):
+def hash_file(file_path: str) -> str:
     h = hashlib.sha256()
     with open(file_path, "rb") as file:
          chunk = file.read(1024)
@@ -9,16 +9,24 @@ def hash_file(file_path):
               chunk = file.read(1024)
     return h.hexdigest()
 
-def verify_integrity(orig_file, dest_file):
+def verify_integrity(orig_file: str, dest_file: str) -> bool:
     orig_hash = hash_file(orig_file)
     dest_hash = hash_file(dest_file)
-    print(f"Integrity check between {orig_file} and {dest_file}:")
-    if orig_hash == dest_hash:
-        return "Files are identical."
-    return "Files are different."
+    return orig_hash == dest_hash
 
 if __name__ == "__main__":
     print("The SHA-256 hash of the file is:", hash_file("sample_files/sample.txt"))
 
-    print(verify_integrity("sample_files/python-powered-w.svg", "sample_files/python-powered-h.svg"))
-    print(verify_integrity("sample_files/python-powered-w.svg", "sample_files/python-powered-w-copy.svg"))
+    orig_file = "sample_files/python-powered-w.svg"
+    dest_file = "sample_files/python-powered-h.svg"
+    print(f"Integrity check between {orig_file} and {dest_file}:")
+    if verify_integrity(orig_file, dest_file):
+        print("Files are identical.")
+    else:
+        print("Files differ.")
+    dest_file = "sample_files/python-powered-w-copy.svg"
+    print(f"Integrity check between {orig_file} and {dest_file}:")
+    if verify_integrity(orig_file, dest_file):
+        print("Files are identical.")
+    else:
+        print("Files differ.")
